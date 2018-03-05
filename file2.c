1642edbd3b63afdcda60914c58ddd93de919837f
Khalid you <k.ihellioun@ump.ac.ma>
2018-03-05 14:51:38 +0100

    Youtube tuts.org.uk

---
+++ file2.c
@@ -0,0 +1,13 @@
+/*
+* @Author: ihelliounkhalid
+* @Date:   2018-03-05 14:42:17
+* @Last Modified by:   ihelliounkhalid
+* @Last Modified time: 2018-03-05 14:46:03
+*/
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+ {
+ 	printf("This is file2");
+ 	return 0;
+ }
